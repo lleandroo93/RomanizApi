5d9b76890030d5b80a1d5e476a3c3ca94b92948d
Leandro Cavalcante <lleandroo93@gmail.com>
2022-11-08 23:19:25 -0400

    return event date

--- src/main/java/br/com/trilhabit/romaniz/model/Evento.java
+++ src/main/java/br/com/trilhabit/romaniz/model/Evento.java
@@ -1,10 +1,13 @@
 package br.com.trilhabit.romaniz.model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -19,5 +22,9 @@ public class Evento implements Serializable {
     private String titulo;
     private String descricao;
     private String observacao;
-    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date data;
+//    private Pessoa pessoa;
+//    private Endereco enderecoDiferente;
+    private String status;
 }

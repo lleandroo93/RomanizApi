accbc33a0591fa0b9e21920024bf00b4ba6aee9c
Leandro Cavalcante <lleandroo93@gmail.com>
2022-11-16 22:38:29 -0400

    Add cors configuration to allow all

--- src/main/java/br/com/trilhabit/romaniz/RomanizApplication.java
+++ src/main/java/br/com/trilhabit/romaniz/RomanizApplication.java
@@ -2,12 +2,26 @@ package br.com.trilhabit.romaniz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RomanizApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RomanizApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+            
+        };
     }
 
 }
